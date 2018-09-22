ffc2f69f7465336ccc416fe47861af2d04c54c3f
Blank <blank@Blank-archlinux.localdomain>
2018-09-22 22:40:24 +0800

    去除直播间限制

--- back.js
+++ back.js
@@ -4,10 +4,6 @@ chrome.browserAction.onClicked.addListener(onClicked);
 function onClicked(tab){
     console.log(1);
     if(tab && tab.url){
-        if(tab.url.search("//egame.qq.com/77777") == -1){
-            alert("仅在德云色直播间内，才可以使用小助手！");
-            return;
-        }
         chrome.tabs.executeScript(tab.id,{
             code: 'egame.start();'
         });
